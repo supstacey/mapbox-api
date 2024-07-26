dbb0676c56f19539fa02499c269764c23ea33518
supstacey <142317181+supstacey@users.noreply.github.com>
2024-07-26 15:59:18 +0100

    Update CompaniesNearTheCurrentRecord.jsx

--- src/app/extensions/CompaniesNearTheCurrentRecord.jsx
+++ src/app/extensions/CompaniesNearTheCurrentRecord.jsx
@@ -24,7 +24,7 @@ const NearestCompanies = ({ context, runServerless, fetchProperties }) => {
       // Request companies batch from serverless function
       const companiesServerlessResponse = await runServerless({
         name: 'getCompaniesWithDistanceBatch',
-        propertiesToSend: ['hs_object_id', 'city', 'state', 'address'],
+        propertiesToSend: ['hs_object_id', 'city', 'address'],
         payload: { batchSize: 30 },
       });
       if (companiesServerlessResponse.status == 'SUCCESS') {
